dfdc6fb01b4149a2d456ed11cce48f2158233427
Alexander Johnston <ajohn432@charlotte.edu>
2024-07-21 12:14:17 -0400

    Adding first C file to the github

---
+++ network.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+void main()
+{
+
+}
