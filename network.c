dc9020280b2ca82ac0f60bee3b3c014166e20909
ajohn432 <ajohn432@charlotte.edu>
2024-07-22 20:14:45 -0400

    Messed around with sys/socket and netinet libraries.

--- network.c
+++ network.c
@@ -5,9 +5,21 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
 
 
 void main()
 {
-
+	struct sockaddr_in addr = {AF_INET, htons(80), inet_addr("192.168.1.1")};
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	printf("%d\n", sock);
+	int result = connect(sock, (struct sockaddr *)&addr, sizeof(addr));
+	if (result == 0)
+	{
+		printf("Success: %d\n", result);		
+	}
+	else
+	{
+		printf("Failure: %d\n", result); 
+	}
 }
